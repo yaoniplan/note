3be1c5232501843145a7ba8a63d46ebcd801b023
yaoniplan <yaoniplan@gmail.com>
2022-09-01 09:54:27 +0800

    remove trash

--- trash/testhundred.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-number = int(input("Enter an integer:"))
-if number <= 100:
-    print("Your number is less than or equal to 100")
-else:
-    print("Your number is greater than 100")
