81ab0415a97821dd13ae333c6bcdde61f4884b21
yaoniplan <yaoniplan@gmail.com>
2022-11-06 12:39:24 +0800

    Update at 2022-11-06-12:39:24

--- .github/autoGit.sh
+++ .github/autoGit.sh
@@ -5,19 +5,19 @@ do
   sleep 20
   git add *
   sleep 20
-  git add .
-  sleep 20
-  git commit -m "Update at `date +%F-%T`"
-  sleep 20
-  git push
-
-  cd ~/grow
-  sleep 20
-  git add *
-  sleep 20
   git add . # Add folder
   sleep 20
   git commit -m "Update at `date +%F-%T`"
   sleep 20
   git push
+
+#  cd ~/grow
+#  sleep 20
+#  git add *
+#  sleep 20
+#  git add . # Add folder
+#  sleep 20
+#  git commit -m "Update at `date +%F-%T`"
+#  sleep 20
+#  git push
 done
