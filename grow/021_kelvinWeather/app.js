e1803d7947aa4bd2bdf1e6c998b989490c4b53f3
yaoniplan <yaoniplan@gmail.com>
2022-11-11 11:40:08 +0800

    Update at 2022-11-11-11:40:08

---
+++ grow/021_kelvinWeather/app.js
@@ -0,0 +1,15 @@
+// Kelvin Weather
+/*
+const kelvin = 0; // Assign value "0" to variable "kelvin"
+let celsius = kelvin - 273; // Convert kelvin to celsius
+let fahrenheit = celsius * (9/5) + 32; // Convert celsius to fahrenheit
+fahrenheit = Math.floor(fahrenheit); // Round down the fahrenheit
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`); // Log the temperature in fahrenheit
+*/
+
+// extra practice
+const kelvin = 293 // Assign value "293" to variable "kelvin"
+let celsius = kelvin - 273; // Convert kelvin to celsius
+let Newton = celsius * (33/100); // Convert celsius to the Newton
+Newton = Math.floor(Newton); // Round down the Newton
+console.log(`The temperature is ${Newton} in Newton.`); // Log the temperature in newton
