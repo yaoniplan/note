066dfc2a616763500fa58eb19ec6c265bddf0566
yaoniplan <yaoniplan@gmail.com>
2022-11-12 10:16:45 +0800

    Update at 2022-11-12-10:16:45

---
+++ grow/022_dogYears/app.js
@@ -0,0 +1,10 @@
+const myAge = 19; // My age
+let earlyYears = 2; // Early years
+earlyYears += 10.5;
+let laterYears = myAge - 2; // Later years
+laterYears *= 4; // Calculate the number of dog years
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears; // Add "earlyYears" and "laterYears" to "myAgeInDogYears"
+let myName = 'Yaoniplan'.toLowerCase(); // Return my name with all lowercase letters
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`) // Display my name and age in dog years
