ad9a4f838a3255a86b6c12b333f8033d035f1ba8
yaoniplan <yaoniplan@gmail.com>
2022-11-14 11:52:32 +0800

    Update at 2022-11-14-11:52:32

--- grow/028_rockPaperScissors/rockPaperScissors.js
+++ grow/028_rockPaperScissors/rockPaperScissors.js
@@ -1,5 +1,13 @@
 let randomNumber = Math.floor(Math.random() * 3);
-console.log(randomNumber);
 
 function getComputerChoice() {
+  if (randomNumber === 0) {
+    console.log("Rock");
+  } else if (randomNumber === 1) {
+    console.log("Paper");
+  } else {
+    console.log("Scissors");
+  }
 }
+
+console.log(getComputerChoice());
