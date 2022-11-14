4828f7e61534412da4e8d8d41f9e413e92606308
yaoniplan <yaoniplan@gmail.com>
2022-11-14 16:05:30 +0800

    Update at 2022-11-14-16:05:30

--- grow/028_rockPaperScissors/rockPaperScissors.js
+++ grow/028_rockPaperScissors/rockPaperScissors.js
@@ -13,16 +13,23 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
   if (playerSelection === "rock" && computerSelection === "Rock") {
     console.log("Evenly matched!");
   } else if (playerSelection === "rock" && computerSelection === "Paper") {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
   } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
   } else if (playerSeleciton === "paper" && computerSelection === "Rock") {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
   } else if (playerSeleciton === "paper" && computerSelection === "Paper") {
+    console.log("Evenly matched");
   } else if (playerSeleciton === "paper" && computerSelection === "Scissors") {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
   } else if (playerSeleciton === "scissors" && computerSelection === "Rock") {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
   } else if (playerSeleciton === "scissors" && computerSelection === "Paper") {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
   } else {
     console.log("Evenly matched");
 }
