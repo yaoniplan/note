b0529f718ca10093f4673c3b484dc0a2e16c0826
yaoniplan <yaoniplan@gmail.com>
2022-11-14 18:37:01 +0800

    Update at 2022-11-14-18:37:01

--- grow/028_rockPaperScissors/rockPaperScissors.js
+++ grow/028_rockPaperScissors/rockPaperScissors.js
@@ -15,37 +15,27 @@ function getComputerChoice() {
 function playRound() {
   if (playerSelection === "rock" && computerSelection === "Rock") {
     console.log("Evenly matched!");
-    break;
   } else if (playerSelection === "rock" && computerSelection === "Paper") {
     console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-    break;
   } else if (playerSelection === "rock" && computerSelection === "Scissors") {
     console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    break;
   } else if (playerSelection === "paper" && computerSelection === "Rock") {
     console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    break;
   } else if (playerSelection === "paper" && computerSelection === "Paper") {
     console.log("Evenly matched!");
-    break;
   } else if (playerSelection === "paper" && computerSelection === "Scissors") {
     console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-    break;
   } else if (playerSelection === "scissors" && computerSelection === "Rock") {
     console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-    break;
   } else if (playerSelection === "scissors" && computerSelection === "Paper") {
     console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    break;
   } else {
     console.log("Evenly matched!");
-    break;
   }
 }
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("What's your selection? rock, paper or scissors?");
     playRound();
   }
 }
