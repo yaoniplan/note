9c1a509e48c2882413709c57605f4e318bfe4d8b
yaoniplan <yaoniplan@gmail.com>
2022-11-14 16:20:49 +0800

    Update at 2022-11-14-16:20:49

--- grow/028_rockPaperScissors/rockPaperScissors.js
+++ grow/028_rockPaperScissors/rockPaperScissors.js
@@ -17,19 +17,19 @@ function playRound() {
   if (playerSelection === "rock" && computerSelection === "Rock") {
     console.log("Evenly matched!");
   } else if (playerSelection === "rock" && computerSelection === "Paper") {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    console.log("You lose! Paper beats rock");
   } else if (playerSelection === "rock" && computerSelection === "Scissors") {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    console.log("You win! Rock beats scissors");
   } else if (playerSelection === "paper" && computerSelection === "Rock") {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    console.log("You win! ${playerSelection} beats ${computerSelection}");
   } else if (playerSelection === "paper" && computerSelection === "Paper") {
     console.log("Evenly matched!");
   } else if (playerSelection === "paper" && computerSelection === "Scissors") {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    console.log("You lose! ${computerSelection} beats ${playerSelection}");
   } else if (playerSelection === "scissors" && computerSelection === "Rock") {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    console.log("You lose! ${computerSelection} beats ${playerSelection}");
   } else if (playerSelection === "scissors" && computerSelection === "Paper") {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    console.log("You win! ${playerSelection} beats ${computerSelection}");
   } else if (playerSelection === "scissors" && computerSelection === "scissors"){
     console.log("Evenly matched!");
   }
