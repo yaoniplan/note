f096658676eeaefd9233907cb395292558182701
yaoniplan <yaoniplan@gmail.com>
2022-11-15 09:50:31 +0800

    Update at 2022-11-15-09:50:31

--- grow/028_rockPaperScissors/rockPaperScissors.js
+++ grow/028_rockPaperScissors/rockPaperScissors.js
@@ -26,6 +26,7 @@ function playRound(palyerSelection, computerSelection) {
 
 let playerScore = parseInt(0);
 let computerScore = parseInt(0);
-const playerSelection = prompt("What's your selection? rock, paper or scissors?");
-const computerSelection = getComputerChoice();
+let playerSelection = prompt("What's your selection? rock, paper or scissors?");
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
 console.log(playRound(playerSelection, computerSelection))
