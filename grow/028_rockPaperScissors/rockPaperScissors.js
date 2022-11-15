67fae8d52e74a1b384a66c3d2d87d75a3a08c71d
yaoniplan <yaoniplan@gmail.com>
2022-11-15 10:46:09 +0800

    Update at 2022-11-15-10:46:09

--- grow/028_rockPaperScissors/rockPaperScissors.js
+++ grow/028_rockPaperScissors/rockPaperScissors.js
@@ -34,7 +34,7 @@ let computerScore = parseInt(0);
 // References: https://stackoverflow.com/questions/52404412/rock-paper-scissors-game-repeating-for-5-rounds
 for (let i = 0; i < 5; i++) {
   let playerSelection = prompt("What's your selection? rock, paper or scissors?");
-  let computerSelection = getComputerChoice();
+  let computerSelection = computerChoice();
   console.log(playRound(playerSelection, computerSelection));
   console.log(`playerScore: ${playerScore}`);
   console.log(`computerScore: ${computerScore}`);
